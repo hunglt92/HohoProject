9569ff7d899a7be7d397900efa7cd8b83aa5a438
Hung FPT <hungtrong@Hungs-MacBook-Pro.local>
2013-06-18 17:27:17 +0700

    version 1.0

---
+++ HohoProject/HohoProjectTests/HohoProjectTests.m
@@ -0,0 +1,36 @@
+//
+//  HohoProjectTests.m
+//  HohoProjectTests
+//
+//  Created by Hung FPT on 6/18/13.
+//  Copyright (c) 2013 Hung FPT. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HohoProjectTests : XCTestCase
+
+@end
+
+@implementation HohoProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
